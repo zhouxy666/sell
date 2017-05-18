6c25336e5950736df7069f186999ae53fb317baa
zhouxy <zhouxy_123@126.com>
2017-05-18 14:56:18 +0800

    创建split小组件

---
+++ src/components/split/split.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="split"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .split
+    height rem(32)
+    background #f3f5f7
+    border-top 1px solid rgba(7, 17, 27, .1)
+    border-bottom 1px solid rgba(7, 17, 27, .1)
+</style>
